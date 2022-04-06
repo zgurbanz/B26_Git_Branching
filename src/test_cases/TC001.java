a536dabd07e43cf3fd1fd65ae6235a5696e9d8db
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-04-06 19:38:39 -0400

    Created By Z

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -3,6 +3,9 @@ package test_cases;
 public class TC001 {
     public static void main(String[] args) {
         // testing some thing
+        //more testing
+        // negative testing
+        //positive testing
 
     }
 }
---
+++ src/test_cases/TC003.java
@@ -0,0 +1,7 @@
+package test_cases;
+
+public class TC003 {
+    //first line
+    //second line
+    //third line
+}
