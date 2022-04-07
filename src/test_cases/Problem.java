05dc87ecf50567927f547b7f6b8dd5fa53b61186
zgurbanz <98862873+zgurbanz@users.noreply.github.com>
2022-04-06 20:08:48 -0400

    Created Problem class

---
+++ src/test_cases/Problem.java
@@ -0,0 +1,11 @@
+package test_cases;
+
+public class Problem {
+    public static void main(String[] args) {
+        for (int j = 0; j < 6; j++) {
+            System.out.println(j);
+
+        }
+    }
+
+}
---
+++ src/test_cases/TC002.java
@@ -0,0 +1,8 @@
+package test_cases;
+
+public class TC002 {
+    public static void main(String[] args) {
+        //Test Case 2
+        //writing smoke
+    }
+}
